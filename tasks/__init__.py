a08e9fa80662643fdb086e48fa2f2e86f7f99eee
eldeivich <47783040+eldeivich@users.noreply.github.com>
2019-12-26 13:30:14 +0100

    Página task funcional agrega tarea a index con boto agregar

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from tasks import routes
+from tasks import routes_copy
--- tasks/routes_copy.py
+++ tasks/routes_copy.py
@@ -6,16 +6,25 @@ import csv
 
 @app.route("/")
 def index():
-    <lee tareas en fichero .data/tareas.dat>
-    return render_template("index.html", tareas)
+    if request.method =='GET':
+        ldatos = open('./data/tareas.dat', 'r')
+        datreader = csv.reader(ldatos, delimiter=',')
 
-@app.route("/newtask", methods['GET', 'POST'])
-def newtask():
+        datos = {}
+        d = []
+
+        for linea in datreader:
+            d.append(linea)
+
+    return render_template("index.html", e=d)
+
+@app.route("/task.html", methods=['GET', 'POST'])
+def task():
     if request.method == 'GET':
         return render_template('task.html')
 
 
-    fdatos = open('./data/tareas.dat', 'w')
+    fdatos = open('./data/tareas.dat', 'a', newline="")
     csvwriter = csv.writer(fdatos, delimiter=",", quotechar='"')
 
     title = request.values.get('title')
